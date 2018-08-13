8495f7eb546debbda9938009f09e58b2c9fb8e5c
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-13 11:34:06 -0500

    Database table exercise

---
+++ database-exercises/albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(255),
+name VARCHAR(255),
+release_date INT UNSIGNED,
+sales DECIMAL(6,2),
+genre VARCHAR(255),
+PRIMARY KEY (id),
+UNIQUE(name,artist)
+);
